2a52fa2bf01f4083d588df8a5e8ff225013c1b43
Gabe Villalobos <gabriel.villalobos@sentry.io>
2026-04-27 10:10:11 -0700

    Wraps validation checks in a feature flag, adds issue reporting

--- src/sentry/options/defaults.py
+++ src/sentry/options/defaults.py
@@ -4082,3 +4082,11 @@ register(
     type=Bool,
     flags=FLAG_NOSTORE,
 )
+
+# Controls strict validation logic in bitbucket installation webhook endpoint.
+register(
+    "integrations.bitbucket.installation-verification.strict",
+    default=False,
+    type=Bool,
+    flags=FLAG_MODIFIABLE_BOOL | FLAG_AUTOMATOR_MODIFIABLE,
+)
